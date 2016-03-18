b0266f5a01c67be59baf1c66f1e9680b799772d0
Evgeny Grishin <e.v.grishin@yandex.ru>
2016-03-19 00:49:36 +0200

    performance fix

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -1,17 +1,16 @@
 <?php
-
+define('_DB_SERVER_', 'mysql91.1gb.ru'); // define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'gb_mh_live'); //define('_DB_NAME_', 'gb_live'); 
+define('_DB_USER_', 'gb_mh_live'); //define('_DB_USER_', 'root'); 
+define('_DB_PASSWD_', '6fd582febsgh'); //define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
 define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
 define('_PS_CACHE_ENABLED_', '0');
-define('_DB_NAME_', 'gb_live');
-define('_MYSQL_ENGINE_', 'InnoDB');
-define('_DB_SERVER_', 'localhost');
-define('_DB_USER_', 'root');
-define('_DB_PREFIX_', 'ps_');
-define('_DB_PASSWD_', '');
 define('_COOKIE_KEY_', 'J8pYbVJ8s7A4RHicYA7vdYCDeXCiydUj9TdLXFaEGpV5rJziGraGmL5Z');
 define('_COOKIE_IV_', 'o0rBzT8x');
 define('_PS_CREATION_DATE_', '2015-05-23');
-define('_RIJNDAEL_KEY_', '0VIYGqrBpWGePIKNzDe9iROyIqugzoVx');
-define('_RIJNDAEL_IV_', 'otrSfEt+6REDvqOn0cTJKA==');
 if (!defined('_PS_VERSION_'))
 	define('_PS_VERSION_', '1.6.0.14');
+define('_RIJNDAEL_KEY_', '0VIYGqrBpWGePIKNzDe9iROyIqugzoVx');
+define('_RIJNDAEL_IV_', 'otrSfEt+6REDvqOn0cTJKA==');
