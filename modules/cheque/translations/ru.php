1312e9c1aab6a3481d3fe68b5039707269a0dd7a
Evgeny Grishin <e.v.grishin@yandex.ru>
2016-10-21 14:29:13 +0300

    cheque module translate

--- modules/cheque/translations/ru.php
+++ modules/cheque/translations/ru.php
@@ -2,14 +2,13 @@
 
 global $_MODULE;
 $_MODULE = array();
-
 $_MODULE['<{cheque}prestashop>cheque_7b4cc4f79be9aae43efd53b4ae5cba4d'] = 'Оплата чеком';
 $_MODULE['<{cheque}prestashop>cheque_14e41f4cfd99b10766cc15676d8cda66'] = 'Этот модуль позволяет вам принимать платежи чеком.';
 $_MODULE['<{cheque}prestashop>cheque_e09484ba6c16bc20236b63cc0d87ee95'] = 'Вы точно хотите удалить эту информацию?';
-$_MODULE['<{cheque}prestashop>cheque_32776feb26ff6f9648054e796aa0e423'] = 'Перед использованием этого модуля настройте поля "Оплата заказа" и "Адрес".';
+$_MODULE['<{cheque}prestashop>cheque_32776feb26ff6f9648054e796aa0e423'] = 'Перед использованием этого модуля настройте поля \"Оплата заказа\" и \"Адрес\".';
 $_MODULE['<{cheque}prestashop>cheque_a02758d758e8bec77a33d7f392eb3f8a'] = 'Не задана валюта для этого модуля.';
-$_MODULE['<{cheque}prestashop>cheque_81c6c3ba23ca2657a8eedc561f865ddb'] = 'Поле "Оплата заказа" обязательное.';
-$_MODULE['<{cheque}prestashop>cheque_00a369029140cfd18857425d49b472f8'] = 'Поле "Адрес" обязательное.';
+$_MODULE['<{cheque}prestashop>cheque_81c6c3ba23ca2657a8eedc561f865ddb'] = 'Поле \"Оплата заказа\" обязательное.';
+$_MODULE['<{cheque}prestashop>cheque_00a369029140cfd18857425d49b472f8'] = 'Поле \"Адрес\" обязательное.';
 $_MODULE['<{cheque}prestashop>cheque_c888438d14855d7d96a2724ee9c306bd'] = 'Настройки обновлены';
 $_MODULE['<{cheque}prestashop>cheque_a60468657881aa431a0a5fccc76258e2'] = 'Оплата чеком';
 $_MODULE['<{cheque}prestashop>cheque_5dd532f0a63d89c5af0243b74732f63c'] = 'Контактная информация';
@@ -32,12 +31,14 @@ $_MODULE['<{cheque}prestashop>payment_execution_7b1c6e78d93817f61f2b1bbc2108a803
 $_MODULE['<{cheque}prestashop>payment_execution_a7a08622ee5c8019b57354b99b7693b2'] = 'Выберите одно из следующих действий:';
 $_MODULE['<{cheque}prestashop>payment_execution_f73ad0f08052884ff465749bf48b55ce'] = 'Оплата чеком возможна в этих валютах:';
 $_MODULE['<{cheque}prestashop>payment_execution_7135ff14c7931e1c8e9d33aff3dfc7f7'] = 'Владелец чека и адрес будут отображены на следующей странице.';
-$_MODULE['<{cheque}prestashop>payment_execution_52f64bc0164b0e79deaeaaaa7e93f98f'] = 'Пожалуйста, подтвердите ваш заказ, нажав на "Я подтверждаю заказ".';
+$_MODULE['<{cheque}prestashop>payment_execution_52f64bc0164b0e79deaeaaaa7e93f98f'] = 'Пожалуйста, подтвердите ваш заказ, нажав на \"Я подтверждаю заказ\".';
 $_MODULE['<{cheque}prestashop>payment_execution_46b9e3665f187c739c55983f757ccda0'] = 'Я подтверждаю заказ';
 $_MODULE['<{cheque}prestashop>payment_execution_569fd05bdafa1712c4f6be5b153b8418'] = 'Другие способы оплаты';
 $_MODULE['<{cheque}prestashop>infos_14e41f4cfd99b10766cc15676d8cda66'] = 'Этот модуль позволяет вам принимать платежи чеком.';
-$_MODULE['<{cheque}prestashop>infos_e444fe40d43bccfad255cf62ddc8d18f'] = 'Если клиент выберет этот способ оплаты, то статус заказа изменится на "Ожидание оплаты"';
+$_MODULE['<{cheque}prestashop>infos_e444fe40d43bccfad255cf62ddc8d18f'] = 'Если клиент выберет этот способ оплаты, то статус заказа изменится на \"Ожидание оплаты\"';
 $_MODULE['<{cheque}prestashop>infos_8c88bbf5712292b26e2a6bbeb0a7b5c4'] = 'Поэтому Вам будет необходимо вручную подтвердить заказ, как только Вы получите чек.';
+$_MODULE['<{cheque}prestashop>payment_4b80fae2153218ed763bdadc418e8589'] = 'Оплата чеком';
+$_MODULE['<{cheque}prestashop>payment_4e1fb9f4b46556d64db55d50629ee301'] = '(обработка заказа продлится дольше)';
 $_MODULE['<{cheque}prestashop>payment_return_88526efe38fd18179a127024aba8c1d7'] = 'Ваш заказ на %s оформлен.';
 $_MODULE['<{cheque}prestashop>payment_return_61da27a5dd1f8ced46c77b0feaa9e159'] = 'Ваш чек должен включать:';
 $_MODULE['<{cheque}prestashop>payment_return_621455d95c5de701e05900a98aaa9c66'] = 'Сумму платежа.';
@@ -50,11 +51,3 @@ $_MODULE['<{cheque}prestashop>payment_return_ffd2478830ca2f519f7fe7ee259d4b96'] 
 $_MODULE['<{cheque}prestashop>payment_return_0db71da7150c27142eef9d22b843b4a9'] = 'По любым вопросам или за дополнительной информацией обратитесь к нашей';
 $_MODULE['<{cheque}prestashop>payment_return_decce112a9e64363c997b04aa71b7cb8'] = 'клиентской службой.';
 $_MODULE['<{cheque}prestashop>payment_return_9bdf695c5a30784327137011da6ef568'] = 'С вашим заказом возникла проблема. Если вы полагаете, что это ошибка, вы можете связаться с нами';
-$_MODULE['<{cheque}prestashop>payment_4b80fae2153218ed763bdadc418e8589'] = 'Оплата чеком';
-$_MODULE['<{cheque}prestashop>payment_4e1fb9f4b46556d64db55d50629ee301'] = '(обработка заказа продлится дольше)';
-$_MODULE['<{cheque}prestashop>payment_execution_0881a11f7af33bc1b43e437391129d66'] = 'Пожалуйста, подтвердите ваш заказ, нажав на "Я подтверждаю заказ"';
-$_MODULE['<{cheque}prestashop>payment_return_d15feee53d81ea16269e54d4784fa123'] = 'Мы обнаружили проблемы с вашим заказом. Если вы считаете, что это ошибка, свяжитесь с нашей';
-$_MODULE['<{cheque}prestashop>payment_f05fd8637f8a6281466a507fcb56baec'] = 'Оплата чеком';
-
-
-return $_MODULE;
