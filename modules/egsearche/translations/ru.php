395b19c2a3df29131150f5aa4b189493b5981b8d
Evgeny Grishin <e.v.grishin@yandex.ru>
2016-11-16 09:44:11 +0200

    translations + images

---
+++ modules/egsearche/translations/ru.php
@@ -0,0 +1,22 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{egsearche}prestashop>displaytop_584de35d6873fe091e8bc3178e5c15c5'] = 'Подбор матраса';
+$_MODULE['<{egsearche}prestashop>displaytop_f7bd60b75b29d79b660a2859395c1a24'] = 'Размер матраса';
+$_MODULE['<{egsearche}prestashop>displaytop_99befcc5c91810c257117f736e7e017d'] = 'Укажите размер';
+$_MODULE['<{egsearche}prestashop>displaytop_ed79acb0cd3d7f8320c53c7798335ef0'] = 'см.';
+$_MODULE['<{egsearche}prestashop>displaytop_599dcce2998a6b40b1e38e8c6006cb0a'] = 'Тип матраса';
+$_MODULE['<{egsearche}prestashop>displaytop_a181a603769c1f98ad927e7367c7aa51'] = 'все';
+$_MODULE['<{egsearche}prestashop>displaytop_a4c8bc6521b23818c6949e5259a92b2f'] = 'Беспружинный';
+$_MODULE['<{egsearche}prestashop>displaytop_689baea52f0e4041836e79dd1dd5dec8'] = 'Пружинные';
+$_MODULE['<{egsearche}prestashop>displaytop_a6f434316500f8216ae72f8ebe4889c5'] = 'Жесткость';
+$_MODULE['<{egsearche}prestashop>displaytop_100b8cad7cf2a56f6df78f171f97a1ec'] = 'Любая';
+$_MODULE['<{egsearche}prestashop>displaytop_80eca96f203362131bc85d4b2bee98f3'] = 'Мягкий';
+$_MODULE['<{egsearche}prestashop>displaytop_0948dd758db70f03cc2c36397e54a065'] = 'Средний';
+$_MODULE['<{egsearche}prestashop>displaytop_31d17c72408b4c3b82f70cc4b1003f0c'] = 'Жесткий';
+$_MODULE['<{egsearche}prestashop>displaytop_736d505aea5fa4259bf0ae5e82e247db'] = 'Подобрать';
+$_MODULE['<{egsearche}prestashop>displaytop_b027168eed2f9d69319d4819454b8ab4'] = 'Ортопедические матрасы по размерам';
+$_MODULE['<{egsearche}prestashop>displaytop_dd5c07036f2975ff4bce568b6511d3bc'] = 'Односпальные матрасы';
+$_MODULE['<{egsearche}prestashop>displaytop_e8cd7da078a86726031ad64f35f5a6c0'] = 'Двуспальные матрасы';
+$_MODULE['<{egsearche}prestashop>displaytop_d2466bd5a5facbe64c9e405e2bfd42df'] = 'Детские матрасы';
